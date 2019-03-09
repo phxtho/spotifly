5a778489577025cdee9d2c934f54307c88d30511
Phatho Pukwana <>
2019-03-10 01:14:22 +0200

    Add project files.

---
+++ Spotifly/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Spotifly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
