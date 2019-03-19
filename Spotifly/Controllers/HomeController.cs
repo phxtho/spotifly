fbe7f0c44a72e1ee9f776951c300ade9cb6464e1
Phatho Pukwana <>
2019-03-19 13:28:52 +0200

    Basic routing for dashboard and statistics
    Added empty views for dashboard and statistics pages
    Added the routing for these pages in the home controller

--- Spotifly/Controllers/HomeController.cs
+++ Spotifly/Controllers/HomeController.cs
@@ -20,6 +20,16 @@ namespace Spotifly.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
