5e09d6d8f34256e838eb77e49d9b64e330696293
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-04-01 09:31:37 +0200

    Remove built in Ms Auth flow

--- Spotifly/Scripts/identity/000_aspnet_roles.sql
+++
@@ -1,9 +0,0 @@
-Use spotifly;
-
-CREATE TABLE `AspNetRoles` (
-    `Id` varchar(767) NOT NULL,
-    `Name` varchar(256) NULL,
-    `NormalizedName` varchar(256) NULL,
-    `ConcurrencyStamp` text NULL,
-    PRIMARY KEY (`Id`)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- Spotifly/Scripts/identity/005_aspnet_user_roles.sql
+++
@@ -1,9 +0,0 @@
-Use spotifly;
-
-CREATE TABLE `AspNetUserRoles` (
-    `UserId` varchar(767) NOT NULL,
-    `RoleId` varchar(767) NOT NULL,
-    PRIMARY KEY (`UserId`, `RoleId`),
-    CONSTRAINT `FK_AspNetUserRoles_AspNetRoles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `AspNetRoles` (`Id`) ON DELETE CASCADE,
-    CONSTRAINT `FK_AspNetUserRoles_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
